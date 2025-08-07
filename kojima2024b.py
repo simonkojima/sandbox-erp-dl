1c3fda5e152d209c2dcd601c9944de7757d61891
serenityprayer627 <serenityprayer627@gmail.com>
2025-08-07 22:15:29 +0200

    update

--- kojima2024b.py
+++ kojima2024b.py
@@ -63,19 +63,28 @@ def func_load_epochs(keywords, mode, epochs):
 
 def func_get_model(X, y):
     _, n_chans, n_times = X.shape
-    F1 = 4
-    D = 2
-    F2 = F1 * D
 
-    model = braindecode.models.EEGNetv4(
-        n_chans=n_chans,
-        n_outputs=2,
-        n_times=n_times,
-        F1=F1,
-        D=D,
-        F2=F2,
-        drop_prob=0.5,
-    )
+    if False:
+        F1 = 4
+        D = 2
+        F2 = F1 * D
+
+        model = braindecode.models.EEGNetv4(
+            n_chans=n_chans,
+            n_outputs=2,
+            n_times=n_times,
+            F1=F1,
+            D=D,
+            F2=F2,
+            drop_prob=0.5,
+        )
+    if True:
+        model = braindecode.models.EEGInceptionERP(
+            n_chans=n_chans,
+            n_outputs=2,
+            n_times=n_times,
+            sfreq=128,
+        )
 
     return model
 
@@ -84,18 +93,18 @@ if __name__ == "__main__":
 
     subject = 1
     tmin = 0.0
-    tmax = 1.0
+    tmax = 1.2
     l_freq = 0.1
-    h_freq = 8
+    h_freq = 40
     order_filter = 2
     resample = 128
 
-    force_epoching = False
+    force_epoching = True
 
-    lr = 1e-3
-    weight_decay = 1e-2
+    # lr = 1e-3
+    # weight_decay = 1e-2
     n_epochs = 500
-    batch_size = 64
+    # batch_size = 64
     patience = 75
     enable_euclidean_alignment = False
     enable_normalization = True
@@ -142,67 +151,77 @@ if __name__ == "__main__":
         )
         epochs.load_data()
 
-    criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([1.0, 3.0]).to(device))
-    # criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([3.0, 1.0]).to(device))
-    # criterion = torch.nn.CrossEntropyLoss()
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR
-    scheduler_params = {"T_max": n_epochs, "eta_min": 1e-6}
-    optimizer = torch.optim.AdamW
-    optimizer_params = {"lr": lr, "weight_decay": weight_decay}
-    early_stopping = rosoku.utils.EarlyStopping(patience=patience)
+    for lr in [1e-2, 1e-3, 1e-4, 1e-5]:
+        for batch_size in [4, 8, 16, 32, 64]:
+            for weight_decay in [1e-1, 1e-2, 1e-3]:
+                criterion = torch.nn.CrossEntropyLoss(
+                    weight=torch.tensor([1.0, 3.0]).to(device)
+                )
+                # criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([3.0, 1.0]).to(device))
+                # criterion = torch.nn.CrossEntropyLoss()
+                scheduler = torch.optim.lr_scheduler.CosineAnnealingLR
+                scheduler_params = {"T_max": n_epochs, "eta_min": 1e-6}
+                optimizer = torch.optim.AdamW
+                optimizer_params = {"lr": lr, "weight_decay": weight_decay}
+                early_stopping = rosoku.utils.EarlyStopping(patience=patience)
 
-    results = rosoku.deeplearning(
-        keywords_train=[f"run:{m}" for m in [1, 3, 5, 8]],
-        keywords_valid=[f"run:10"],
-        keywords_test=[f"run:12"],
-        func_load_epochs=functools.partial(func_load_epochs, epochs=epochs),
-        func_convert_epochs_to_ndarray=functools.partial(
-            rosoku.utils.convert_epochs_to_ndarray,
-            label_keys={"marker:Target": 1, "marker:NonTarget": 0},
-        ),
-        apply_func_proc_per_obj=True,
-        batch_size=batch_size,
-        n_epochs=n_epochs,
-        criterion=criterion,
-        optimizer=optimizer,
-        optimizer_params=optimizer_params,
-        func_get_model=func_get_model,
-        scheduler=scheduler,
-        scheduler_params=scheduler_params,
-        device=device,
-        enable_ddp=enable_ddp,
-        func_proc_epochs=func_proc_epochs,
-        early_stopping=early_stopping,
-        enable_normalization=enable_normalization,
-        name_classifier="eegnet4.2",
-        history_fname=(save_base / "history" / f"sub-{subject}"),
-        checkpoint_fname=(save_base / "checkpoint" / f"sub-{subject}.pth"),
-        desc="eegnet4.2/drop_prob=0.25",
-        enable_wandb_logging=True,
-        wandb_params={
-            "config": {
-                "lr": lr,
-                "weight_decay": weight_decay,
-                "n_epochs": n_epochs,
-                "force_epoching": force_epoching,
-                "device": device,
-            },
-            "project": "sandbox-kojima2024b",
-            "name": f"sub-{subject}",
-        },
-        seed=seed,
-    )
+                results = rosoku.deeplearning(
+                    keywords_train=[f"run:{m}" for m in [1, 3, 5, 8]],
+                    keywords_valid=[f"run:10"],
+                    keywords_test=[f"run:12"],
+                    func_load_epochs=functools.partial(
+                        func_load_epochs, epochs=epochs.copy()
+                    ),
+                    func_convert_epochs_to_ndarray=functools.partial(
+                        rosoku.utils.convert_epochs_to_ndarray,
+                        label_keys={"marker:Target": 1, "marker:NonTarget": 0},
+                    ),
+                    apply_func_proc_per_obj=True,
+                    batch_size=batch_size,
+                    n_epochs=n_epochs,
+                    criterion=criterion,
+                    optimizer=optimizer,
+                    optimizer_params=optimizer_params,
+                    func_get_model=func_get_model,
+                    scheduler=scheduler,
+                    scheduler_params=scheduler_params,
+                    device=device,
+                    enable_ddp=enable_ddp,
+                    func_proc_epochs=func_proc_epochs,
+                    early_stopping=early_stopping,
+                    enable_normalization=enable_normalization,
+                    name_classifier="EEGInceptionERP",
+                    history_fname=(save_base / "history" / f"sub-{subject}"),
+                    checkpoint_fname=(save_base / "checkpoint" / f"sub-{subject}.pth"),
+                    desc="EEGInceptionERP/drop_prob=0.25",
+                    enable_wandb_logging=True,
+                    wandb_params={
+                        "config": {
+                            "lr": lr,
+                            "weight_decay": weight_decay,
+                            "n_epochs": n_epochs,
+                            "batch_size": batch_size,
+                            "force_epoching": force_epoching,
+                            "device": device,
+                            "subject": subject,
+                            "model": "EEGInceptionERP",
+                        },
+                        "project": "sandbox-kojima2024b-params-search",
+                        "name": f"sub-{subject}",
+                    },
+                    seed=seed,
+                )
 
-    print(results.loc[0])
+                print(results.loc[0])
 
-    probas = results.loc[0]["probas"]
-    labels = results.loc[0]["labels"]
-    preds = results.loc[0]["preds"]
+                probas = results.loc[0]["probas"]
+                labels = results.loc[0]["labels"]
+                preds = results.loc[0]["preds"]
 
-    f1 = sklearn.metrics.f1_score(labels, preds)
-    bacc = sklearn.metrics.balanced_accuracy_score(labels, preds)
+                f1 = sklearn.metrics.f1_score(labels, preds)
+                bacc = sklearn.metrics.balanced_accuracy_score(labels, preds)
 
-    auc = sklearn.metrics.roc_auc_score(labels, probas[:, 1])
-    print(f"BACC: {bacc}")
-    print(f"F1: {f1}")
-    print(f"AUC: {auc}")
+                auc = sklearn.metrics.roc_auc_score(labels, probas[:, 1])
+                print(f"BACC: {bacc}")
+                print(f"F1: {f1}")
+                print(f"AUC: {auc}")
